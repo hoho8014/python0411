7d6048b40ab07f5d026f7782680b233fb72ce619
hoho8014 <lmh80@hanmail.net>
2022-04-12 10:39:05 +0900

    루프 연습

--- DemoLoop.py
+++ DemoLoop.py
@@ -6,4 +6,21 @@ while Value > 0:
     Value -=1
 
 print("전체 코드 실행 종료")
- 
+
+#반복구문을 원하는 만큼 
+for i in range(5):
+    print(i)
+
+lst = ["apple", 100, 3.14]
+for item in lst:
+    print(item, type(item))
+
+d = {"apple":100, "kiwi":200}
+for k,v in d.items():
+    print(k,v)
+
+#구구단 출력
+for i in [2,3,4,5,6]:
+    print("---{0}단---".format(i))
+    for j in [1,2,3,4,5,6,7,8,9]:
+        print("{0} * {1} = {2}".format(i,j,i*j))
