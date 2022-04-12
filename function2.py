207879908dd050df3d8d40aca29b9393ddf4d82d
hoho8014 <lmh80@hanmail.net>
2022-04-12 13:43:25 +0900

    오후

--- function2.py
+++ function2.py
@@ -1,22 +1,38 @@
-# function2.py
+# function2.py 
 #전역변수
-x = 1
+x = 1 
 def func(a):
-    return x+a
+    return x+a 
 
 #호출
 print(func(1))
 
-def func(a):
+def func2(a):
     #지역변수
-    x = 2
-    return x+a
-    
+    x = 2 
+    return x+a 
+
 #호출
-print(func 2(1))
+print(func2(1))
 
 #불변형식
 a = 1.2
 print(id(a))
-a = 2.3
+a = 2.3 
 print(id(a))
+print("---가변형식---")
+lst = [1,2,3]
+print(id(lst))
+lst.append(4)
+print(id(lst))
+
+#불변형식인데 함수 내부에서 읽기 + 쓰기 
+g = 1 
+def testScope(a):
+    #global g 
+    g = 2 
+    return g+a 
+
+#호출
+print(testScope(1))
+print("전역변수 g:", g)
