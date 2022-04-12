b48e43ab93070daff6d1b31389053eb0ed5d9ef8
hoho8014 <lmh80@hanmail.net>
2022-04-12 13:10:32 +0900

    함수 정의

--- function1.py
+++ function1.py
@@ -15,3 +15,18 @@ def swap(x,y):
 
 #호출
 print(swap(2,3))
+
+#디버깅 연습 함수 정의
+def intersect(prelist, postlist):
+    #함수 내부의 지역변수를 초기화(List)
+    retlist = []
+    #H(0) | A(1) | M(2)
+    for x in prelist:
+        #특정 글자가 positlist에 있고 특정 글자가 아직 없다(retList)
+        if x in postlist and x not in retlist:
+            retlist.append(x)
+    return retlist
+
+#함수를 호출
+print(intersect("HAM","SPAM"))
+
