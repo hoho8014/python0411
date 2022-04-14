7123d66ef1367be3b8dd20e3a517b719ff907776
hoho8014 <lmh80@hanmail.net>
2022-04-14 11:31:00 +0900

    로그 파일 에러

--- doc_copy_readline2.py
+++ doc_copy_readline2.py
@@ -1,6 +1,9 @@
+#8장에서 open()함수 9장에서 정규표현식
 import re 
 
+#로그파일의 위치(원본)
 f=open('c:\\work\\PV3.txt','rt', encoding='utf-8')
+#복사본
 g=open('c:\\work\\PV3_copy.txt','wt', encoding='utf-8')
 
 #많은 라인의 파일이면 
@@ -8,7 +11,8 @@ g=open('c:\\work\\PV3_copy.txt','wt', encoding='utf-8')
 #파일의 EOF(End Of File)이 아니면 계속 읽도록 한다. 
 line = f.readline()
 while (line != ''):
-    if (re.search("\d{4}", line)):
+    #에러가 발생
+    if (re.search("error", line)):
         g.write(line + "\n")
     line = f.readline()
 
