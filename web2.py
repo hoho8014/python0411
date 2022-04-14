8c5c9703f697fbc2cca52feb6e3839890fcb056e
hoho8014 <lmh80@hanmail.net>
2022-04-14 14:42:21 +0900

    웹크롤링 2

--- web2.py
+++ web2.py
@@ -4,13 +4,20 @@ import urllib.request
 #크롤링
 from bs4 import BeautifulSoup
 
-data = urllib.request.urlopen("http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri")
-soup = BeautifulSoup(data, "html.parser")
-
-# <td class="title">
-# 	<a href="/webtoon/detail">마음의 소리 50화 <격렬한 나의 하루> </a>
-# </td>
-cartoons = soup.find_all("td", class_="title")
-print("개수:{0}".format(len(cartoons)))
-print(cartoons[0].find("a").text)
-print(cartoons[0].find("a")["href"])
+#파일 생성
+f = open("c:\\work\\webtoon.txt", "wt", encoding="utf-8")
+#수열함수로 페이지 번호 생성
+for i in range(1,6):
+    url = \
+    "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" \
+    + str(i)
+    print(url)
+    data = urllib.request.urlopen(url)
+    #검색이 용이한 객체 
+    soup = BeautifulSoup(data, "html.parser")
+    cartoons = soup.find_all("td", class_="title")
+    for item in cartoons:
+        tag = item.find("a")
+        print(tag.text.strip())
+        f.write(tag.text.strip() + "\n")
+f.close()
