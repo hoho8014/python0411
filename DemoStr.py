9cc65ba3b72594c427711b45a44ce7bffbb24dbf
hoho8014 <lmh80@hanmail.net>
2022-04-14 10:27:13 +0900

    문자열 처리함수

--- DemoStr.py
+++ DemoStr.py
@@ -7,3 +7,25 @@ print(strA.capitalize() )
 print(strA.endswith("ful"))
 print(strA.count("p"))
 print(strA.count("p",7))
+print("MBC2580".isalnum())
+print("MBC:2580".isalnum())
+print("2580".isnumeric())
+
+names = ["전우치","홍길동","이순신"]
+for name in names:
+    print("안녕하세요 {0}님 더운 여름입니다.".format(name))
+    print("=" * 40)
+
+u = "<<<   spam and ham    >>>"
+result = u.strip("<> ")
+print(u)
+print(result)
+
+result2 = result.replace("spam", "spam egg")
+print(result2)
+lst = result2.split()
+print(lst)
+print("---하나로 합치기---")
+print(":)".join(lst))
+print(" ".join(lst))
+
