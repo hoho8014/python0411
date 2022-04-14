31809275a851473117a16fc9e7902caf56dbc58b
hoho8014 <lmh80@hanmail.net>
2022-04-14 10:15:25 +0900

    데모 str

---
+++ DemoStr.py
@@ -0,0 +1,9 @@
+# DemoStr.py 
+#print(dir(str))
+
+strA = "python is very powerful"
+print(len(strA))
+print(strA.capitalize() )
+print(strA.endswith("ful"))
+print(strA.count("p"))
+print(strA.count("p",7))
